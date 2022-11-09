876b5e2b816c0c16b9b36b060bdce20bf6af2351
Daniel <yunlew531@gmail.com>
2022-11-10 02:07:48 +0800

    加入 utils trimDeltaOps

--- src/utils/quillDeltaToHtml.ts
+++ src/utils/quillDeltaToHtml.ts
@@ -1,12 +1,31 @@
 import { DeltaOperation } from 'quill';
 import { Quill } from 'react-quill';
 
+export const trimDeltaOps = (deltaOps: DeltaOperation[]) => {
+  const lastOpIdx = deltaOps.length - 1;
+  const trimDeltaOpsTemp = deltaOps?.reduce((prev, op, idx) => {
+    if (idx === lastOpIdx && typeof op.insert === 'string') {
+      const insertSplitArr = op.insert.split('').reverse();
+      const lastNewLineSignIdx = insertSplitArr.findIndex((opSplit: string) => opSplit !== '\n');
+      insertSplitArr.splice(0, lastNewLineSignIdx);
+      const trimInsert = insertSplitArr.reverse().join('');
+      prev.push({ insert: trimInsert });
+    } else prev.push(op);
+
+    return prev;
+  }, [] as DeltaOperation[]);
+
+  return trimDeltaOpsTemp;
+};
+
 export default (deltaOps: DeltaOperation[]) => {
   if (!deltaOps) return null;
+  const trimDeltaOpsTemp = trimDeltaOps(deltaOps);
   const container = document.createElement('div');
   const quill = new Quill(container, { readOnly: true });
   const delta = quill.getContents();
-  delta.ops = deltaOps;
+
+  delta.ops = trimDeltaOpsTemp;
   quill.setContents(delta);
   return container;
 };
